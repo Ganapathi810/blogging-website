bc457de4758082b0c530df13eb8d2d12e2f248cf
Ganapathi Othoju <othojuganapathi123@gmail.com>
2025-07-20 19:05:51 +0530

    Update auth.ts

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -16,7 +16,7 @@ export const { auth, handlers, signIn, signOut } = NextAuth({
         })
 
     ],
-    secret : process.env.AUTH_SECRET,
+    secret : process.env.NEXTAUTH_SECRET,
     session : {
         strategy : 'jwt'
     },
