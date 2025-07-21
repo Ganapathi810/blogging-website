611f8e684d1852fae72e8bcf54ef183570df2a81
O V V S Ganapathi <othojuganapathi123@gmail.com>
2025-07-21 19:17:06 +0530

    commendted out some logs

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,19 +4,19 @@ import { getToken } from 'next-auth/jwt'
 
 
 export default async function middleware(request : NextRequest) {
-    console.log(`[Middleware] Running for path: ${request.nextUrl.pathname}`);
+    // console.log(`[Middleware] Running for path: ${request.nextUrl.pathname}`);
 
-    const token = await getToken({
-        req : request,
-        secret : process.env.AUTH_SECRET
-    })
+    // const token = await getToken({
+    //     req : request,
+    //     secret : process.env.AUTH_SECRET
+    // })
 
 
-    if(!token) {
-        console.log(`[Middleware] No token found for ${request.nextUrl.pathname}.`);
-        return NextResponse.redirect(new URL('/api/auth/signin',request.url))
-    }
-    console.log(`[Middleware] token found for ${request.nextUrl.pathname}. and proceeding ahead`);
+    // if(!token) {
+    //     console.log(`[Middleware] No token found for ${request.nextUrl.pathname}.`);
+    //     return NextResponse.redirect(new URL('/api/auth/signin',request.url))
+    // }
+    // console.log(`[Middleware] token found for ${request.nextUrl.pathname}. and proceeding ahead`);
 
     return NextResponse.next()
 }
